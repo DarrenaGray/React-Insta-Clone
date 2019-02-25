1a62111f533873bb26c646e0738aaa1b4739d0dc
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-02-25 16:13:03 -0500

    dummyData successfully imported and held in state

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 import dummyData from './dummy-data';
+import SearchBar from './components/SearchBar/SearchBar';
 import './App.css';
 
 class App extends React.Component {
+  constructor () {
+    super ();
+    this.state = {
+      dummyData: dummyData
+    };
+  }
   render() {
     return (
       <div className="App">
-
+        <SearchBar />
       </div>
     );
   }
