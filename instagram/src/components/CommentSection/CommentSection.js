5d57f293bbf40e6bb792d0bab8efd9bed82c160b
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-02-27 17:37:24 -0500

    Refactored Comment Section from a functional component to class component for comments and likes

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,21 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+// import * as Icon from 'react-feather';
 
-const CommentSection = props => {
-    return (
-        <div className="comments">
-            {props.data.comments.map(data => {
+class CommentSection extends React.Component {
+    constructor(props) {
+        super(props); 
+           this.state = {
+               comments: props.comments,
+               likes: props.likes
+           }
+        }
+
+    render(){
+        return (
+        <div className="comment-section">
+            <div className="commentsAndLikes">
+            {/* <Icon.Heart className='icons' /> */}
+            </div>
+             <p>{this.state.likes} likes</p>
+            {this.state.comments.map((comment, i) => {
                 return (
-                    <div className="comment-content">
-                        <p>{data.username}:</p>
-                        <p>{data.text}</p>
+                    <div className="comment-content" key={i}>
+                        <p>{comment.username}:</p>
+                        <p>{comment.text}</p>
                     </div>
                 )
                 })}
         </div>
     )
     }
-
+}
 // PropTypes
 
 CommentSection.propTypes = {
