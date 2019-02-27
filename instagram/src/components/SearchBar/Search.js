25076e1f35e781a495bf3ed752201bce95ff251a
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-02-27 15:53:21 -0500

    Installed reactstrap to replace icon image file imports

--- instagram/src/components/SearchBar/Search.js
+++ instagram/src/components/SearchBar/Search.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import './SearchBar.css';
+
+// import * as Icon from 'react-feather';
+
 
 const Search = () => {
     return (
--- instagram/src/components/SearchBar/SearchContainer.js
+++ instagram/src/components/SearchBar/SearchContainer.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './SearchBar.css';
 import Search from './Search';
-
-import IGicon from '../../assets/ig_logo_icon.png';
 import IGlogo from '../../assets/ig_logo.png';
+
+// import * as Icon from 'react-feather';
 
 const SearchBar = () => {
     return (
         <div className="ig-imgs">
           <div>
-            <img className="ig-camera" src={IGicon} alt={IGicon} />
+            <img className="ig-camera" src="" alt="" />
             <img className="ig-logo"src={IGlogo} alt={IGlogo}/>
             </div>
             <div>
-                <img className="ig-icons" />
-                <img className="ig-icons" />
-                <img className="ig-icons" />
+                <img className="ig-icons" src="" alt="" />
+                <img className="ig-icons" src="" alt="" />
+                <img className="ig-icons" src="" alt="" />
             </div>
             <Search />
         </div>
