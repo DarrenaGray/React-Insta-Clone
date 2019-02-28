d20b325513d9ce43511b09f67a68b02a1d79cf88
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-02-28 17:49:55 -0500

    Transferred data from App.js to PostPage.js and imported PostPage to App

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,44 +1,49 @@
 import React from 'react';
 
 import './App.css';
-import dummyData from './dummy-data';
-import SearchBar from './components/SearchBar/SearchBar';
-import PostContainer from './components/PostContainer/PostContainer';
-// import PostPage from './components/PostContainer/PostPage';
-import Login from './components/Login/Login';
+// import dummyData from './dummy-data';
+// import SearchBar from './components/SearchBar/SearchBar';
+// import PostContainer from './components/PostContainer/PostContainer';
+import PostPage from './components/PostContainer/PostPage';
+// import Login from './components/Login/Login';
+// import withAuthenticate from './components/Authentication/withAuthenticate';
+
+// const ComponentFromWithAuthenticate = withAuthentication(PostPage)();
 
 class App extends React.Component {
   constructor () {
     super ();
-    this.state = {
-      dummyData: [],
-      searchData: []
+    this.state = {};
+      // showFirst: true,
+      // dummyData: [],
+      // searchData: []
     };
-  }
+  // }
 
-  componentDidMount() {
-    console.log('CMD');
-    this.setState({
-      dummyData: dummyData
-    })
-  }
+  // componentDidMount() {
+  //   console.log('CMD');
+  //   this.setState({
+  //     dummyData: dummyData
+  //   })
+  // }
 
-  searchBarHandler = e => {
-    const posts = this.state.dummyData.filter( post => {
-      if(post.username.includes(e.target.value)) {
-        return post
-      } else {
-        return null
-      }
-    });
-    this.setState({
-      searchData: posts
-    })
-  }
+  // searchBarHandler = e => {
+  //   const posts = this.state.dummyData.filter( post => {
+  //     if(post.username.includes(e.target.value)) {
+  //       return post
+  //     } else {
+  //       return null
+  //     }
+  //   });
+  //   this.setState({
+  //     searchData: posts
+  //   })
+  // }
 
   render() {
     return (
       <div className="App">
+        {/* <Login />
         <SearchBar searchPosts={this.searchBarHandler}/>
         <PostContainer 
             postData={
@@ -46,9 +51,8 @@ class App extends React.Component {
               this.state.searchData :
               this.state.dummyData
             }
-        />
-        {/* <PostPage /> */}
-        <Login />
+        /> */}
+        <PostPage />
       </div>
     );
   }
--- instagram/src/components/PostContainer/PostPage.js
+++ instagram/src/components/PostContainer/PostPage.js
@@ -1,7 +1,55 @@
 import React from 'react';
 
-class PostPage extends React.Component {
+import dummyData from '../../dummy-data';
+import SearchBar from '../SearchBar/SearchBar';
+import PostContainer from '../PostContainer/PostContainer';
+import Login from '../Login/Login';
 
+class PostPage extends React.Component {
+    constructor (props) {
+        super (props);
+        this.state = {
+          // showFirst: true,
+          dummyData: [],
+          searchData: []
+        };
+      }
+    
+      componentDidMount() {
+        console.log('CMD');
+        this.setState({
+          dummyData: dummyData
+        })
+      }
+    
+      searchBarHandler = e => {
+        const posts = this.state.dummyData.filter( post => {
+          if(post.username.includes(e.target.value)) {
+            return post
+          } else {
+            return null
+          }
+        });
+        this.setState({
+          searchData: posts
+        })
+      }
+    
+      render() {
+        return (
+          <div className="App">
+            <Login />
+            <SearchBar searchPosts={this.searchBarHandler}/>
+            <PostContainer 
+                postData={
+                  this.state.searchData.length > 0 ?
+                  this.state.searchData :
+                  this.state.dummyData
+                }
+            />
+          </div>
+        );
+      }
 }
 
 export default PostPage;
